42bee9c93b3669c872cc54dc5d031a7830551922
Alois <moaugustin@proton.me>
2026-03-10 20:58:10 +0100

    Moved client part to Client monorepo

--- web/values.ts
+++
@@ -1,5 +0,0 @@
-export const RESPONSE_TIMEOUT = 15_000;
-export const RETRY_COUNT = 10;
-export const RETRY_INTERVAL = 3_000;
-export const PING_INTERVAL = 5_000;
-export const TRANSPORT_URL = "https://methanium.net:959";
